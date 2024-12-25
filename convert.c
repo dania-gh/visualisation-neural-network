7b740cb9430eec0217f0441bd03f7a3c1e987bf4
Belhaddad dania <belhaddad2023@gmail.com>
2024-12-25 22:13:17 +0100

    modification de la fonction matrix_csv dans le fichier convert.c

--- convert.c
+++ convert.c
@@ -1,26 +1,23 @@
 #include"convert.h"
 #include"matrice.h"
 
-void matrix_csv(const char *filename,int cols, int lin, matrix* mat)
-{
+void matrix_csv(const char *filename, int cols, int lin, matrix* mat) {
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        printf("Erreur d'ouverture du fichier\n");
+        printf("Erreur d'ouverture du fichier %s\n", filename);
         exit(EXIT_FAILURE);
     }
-
     for (int i = 0; i < lin; i++) {
         for (int j = 0; j < cols; j++) {
-            if (fscanf(file, "%lf,", mat->values[i][j]) != 1) {
-                printf("Erreur de lecture du fichier\n");
+            if (fscanf(file, "%lf,", &mat->values[i][j]) != 1) {
                 fclose(file);
                 exit(EXIT_FAILURE);
             }
         }
     }
-
     fclose(file);
 }
+
 
 
 void creat_X_Y(matrix* X, matrix* Y)
