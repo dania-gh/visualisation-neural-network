295a7c98beb55bd164e15d99b9a67036c6397ac8
Belhaddad dania <belhaddad2023@gmail.com>
2024-12-25 19:30:23 +0100

    update main.c

--- main.c
+++ main.c
@@ -3,10 +3,22 @@
 
 #include "convert.h"
 #include "matrice.h"
+void printMatrix(matrix* m) {
+    for (int i = 0; i < m->row; i++) {
+        for (int j = 0; j < m->col; j++) {
+            printf("%f ", m->values[i][j]);
+        }
+        printf("\n");
+    }
+}
 int main ()
 {
     matrix* X = createMatrix(100, 2);
     matrix* Y = createMatrix(100 ,1);
 
+    creat_X_Y(X, Y);
 
+    printf("Matrice 1:\n");
+    printMatrix(X);
+    
 }
