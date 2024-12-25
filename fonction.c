d177c7ae5a831cd3614125dee3b25b9734375613
Belhaddad dania <belhaddad2023@gmail.com>
2024-12-25 18:46:41 +0100

    ajout de la fonction log_loss

--- fonction.c
+++ fonction.c
@@ -9,3 +9,10 @@ double sigmoid_deriv(double z)
 {
     return z*(1.0 -z);
 }
+
+double log_loss(double expected, double predicted) 
+{
+    const double epsilon = 1e-15;
+    predicted = fmax(fmin(predicted ,1-epsilon), epsilon);
+    return -(expected * log(predicted)+(1-expected)*log(1- predicted));
+}
