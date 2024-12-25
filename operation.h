98a8dce1f81bd5ddd25c9b3f35bd168ebd83c3f2
Ferielysf <ysfferiel@gmail.com>
2024-12-25 16:09:39 +0100

    update operation.h

--- operation.h
+++ operation.h
@@ -6,6 +6,14 @@
 #include <math.h>
 #include "matrice.h"
 
+matrix* multiplication (matrix* m1, matrix* m2);
+matrix* addition (matrix* m1, matrix* m2);
+matrix* subtraction (matrix* m1, matrix* m2);
+matrix* dot (matrix* m1, matrix* m2);
+matrix* multi_scaler (matrix* m , double n );
+matrix* add_scaler (matrix* m , double n );
+matrix* matrix_exp(matrix* m);
+
 
 
 #endif
