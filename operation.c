19646463fc9a141f4c6e058adc5b6cbc363acf66
Ferielysf <ysfferiel@gmail.com>
2024-12-25 14:59:51 +0100

    create multiplication fonction

--- operation.c
+++ operation.c
@@ -1,1 +1,19 @@
 #include "operation.h"
+
+
+matrix* multiplication (matrix* m1, matrix* m2)
+{
+    matrix* m = createMatrix(m1->row, m1->col);
+
+    for (int i =0 ; i <m1->row ; i++)
+    {
+        for(int j= 0 ; j<= m1->col ;j++)
+        {
+            m->values[i][j] = m1->values[i][j] * m1->values[i][j];
+        }
+    }
+
+    return m;
+}
+
+
