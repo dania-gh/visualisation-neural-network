8ecb2a948de22dfd2baf521edb9dd82dc3005548
Belhaddad dania <belhaddad2023@gmail.com>
2024-12-25 23:11:53 +0100

    creation de la matrice des Weights

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include "convert.h"
 #include "matrice.h"
 void printMatrix(matrix* m) {
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < m->row; i++) {
         for (int j = 0; j < m->col; j++) {
             printf("Valeur à [%d][%d] : %f\n", i, j, m->values[i][j]);
         }
@@ -13,12 +13,17 @@ void printMatrix(matrix* m) {
 }
 int main ()
 {
+    srand(time(NULL));
     matrix* X = createMatrix(100, 2);
     matrix* Y = createMatrix(100 ,1);
 
     creat_X_Y(X, Y);
 
-    printf("Matrice 1:\n");
-    printMatrix(X);
+    
+
+    matrix* hidden_weights = createMatrix(2,2);
+    matrix_randomize(hidden_weights ,2);
+
+    printMatrix(hidden_weights);
 
 }
