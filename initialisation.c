fbc0a501fc7d029f8b0976b46fc0177ba28e8570
Ferielysf <ysfferiel@gmail.com>
2024-12-25 10:01:59 +0100

    change labels

--- initialisation.c
+++ initialisation.c
@@ -1,7 +1,7 @@
 #include "initialisation.h"
 
 
-Parameters* parameters(int nb_x,int nb_y, int nb_node, int nb_layers)
+Parameters* initialisation(int nb_x,int nb_y, int nb_node, int nb_layers)
 {
     Parameters* p= (Parameters*)malloc(sizeof(Parameters));
 
