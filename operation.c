3da6a30966bde618b0b1fb5360b0b60bd99a6bca
Ferielysf <ysfferiel@gmail.com>
2024-12-25 15:02:47 +0100

    create subtruction fonction

--- operation.c
+++ operation.c
@@ -31,3 +31,19 @@ matrix* addition (matrix* m1, matrix* m2)
 
     return m;
 }
+
+
+matrix* subtraction (matrix* m1, matrix* m2)
+{
+    matrix* m = createMatrix(m1->row, m1->col);
+
+    for (int i = 0 ; i <m1->row ; i++)
+    {
+        for(int j = 0 ; j<= m1->col ;j++)
+        {
+            m->values[i][j] = m1->values[i][j] - m1->values[i][j];
+        }
+    }
+
+    return m;
+}
