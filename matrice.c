72ddac1f5a4f03049a860c8244bd3f48d96f4071
Ferielysf <ysfferiel@gmail.com>
2024-12-25 09:04:49 +0100

    create matrix fonction

--- matrice.c
+++ matrice.c
@@ -1,1 +1,16 @@
 #include "matrice.h"
+
+
+matrix* createMatrix(int row,int col)
+{
+    matrix* matrice=malloc(sizeof(matrix));
+    matrice->col=col;
+    matrice->row=row;
+    matrice->values=malloc(row*sizeof(double));
+    for(int i=0;i<row;i++)
+    {
+        matrice->values[i]=malloc(col*sizeof(double));
+    }
+    return matrice;
+
+}
