5a9a824ad91ecc6b87eb4070f9394bd66faed243
Belhaddad dania <belhaddad2023@gmail.com>
2024-12-25 19:01:58 +0100

    alocation pour les deux matrices X et Y

--- main.c
+++ main.c
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "convert.h"
+#include "matrice.h"
+int main ()
+{
+    matrix* X = createMatrix(100, 2);
+    matrix* Y = createMatrix(100 ,1);
+
+
+}
