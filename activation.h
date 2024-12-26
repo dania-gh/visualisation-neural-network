8fff933629f7864da487e39170aef57a5c52b7ec
Ferielysf <ysfferiel@gmail.com>
2024-12-26 11:33:37 +0100

    add libraries to activation file

---
+++ activation.h
@@ -0,0 +1,14 @@
+#ifndef activation_h
+#define activation_h
+
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include "matrice.h"
+#include "initialisation.h"
+#include "operation.h"
+
+
+
+#endif
