c8fd7e3b23efc4b4fb4992c223ddb956e39a17fe
Ferielysf <ysfferiel@gmail.com>
2024-12-26 12:15:42 +0100

    start activation fonction

--- activation.c
+++ activation.c
@@ -1,1 +1,30 @@
 #include "activation.h"
+
+
+
+activation* segmoid (matrix* x, Parameters* p)
+{
+    activation* A = (activation*) malloc(p->num_layers * sizeof(activation));
+
+    A->nb_layers = p->num_layers;
+    
+    A->activ = (matrix*) malloc(A->nb_layers * sizeof(matrix));
+    
+    for (int i = 0; i < p->num_layers ; i++)
+    {
+        if(i==0)
+        {
+            matrix* z = dot(p->weight[i],x);
+            z = addition(z,p->bias[i]);
+        }
+        else 
+        {
+            matrix* z = dot(p->weight[i],A->activ[i-1]);
+            z = addition(z,p->bias[i]);
+        }
+
+        
+
+    }
+
+}
