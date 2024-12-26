372376d7ff1bf2aed567345867cbc36a2c63a2f9
Belhaddad dania <belhaddad2023@gmail.com>
2024-12-26 10:29:49 +0100

    modif matrice output_bias et hidden_bias avec des 0

--- main.c
+++ main.c
@@ -27,24 +27,27 @@ int main ()
     
 
     matrix* hidden_bias=createMatrix(2,1);
-    matrix_randomize(hidden_bias ,2);
+    matrix_fill(hidden_bias,0);
+    printMatrix(hidden_bias);
     
+    print("\n");
 
     matrix* output_weights=createMatrix(2,1);
     matrix_randomize(output_weights,1);
    
 
     matrix* output_bias=createMatrix(1,1);
-    matrix_randomize(output_bias,1);
+    matrix_fill(output_bias,0);
+    printMatrix(output_bias);
     
 
-    for (int epoch=0 ; epoch < epochs ; epoch++)
+    /*for (int epoch=0 ; epoch < epochs ; epoch++)
     {
         for (int i=0 ; i <100 ; i++)
         {
             
         }
-    } 
+    } */
     
 
 }
