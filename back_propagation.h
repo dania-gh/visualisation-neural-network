c4d12ad8b6ba96bffd7aeab5046ed2081ed9a9ee
Ferielysf <ysfferiel@gmail.com>
2024-12-27 14:32:19 +0100

    add libraries

---
+++ back_propagation.h
@@ -0,0 +1,15 @@
+#ifndef back_propagation_h
+#define back_propagation_h
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include "matrice.h"
+#include "initialisation.h"
+#include "operation.h"
+#include "activation.h"
+
+
+
+
+#endif
