89180a804f179890535fa3ec03cf246c50166f36
Ferielysf <ysfferiel@gmail.com>
2024-12-27 14:46:38 +0100

    create back propagtion fonction

--- back_propagation.c
+++ back_propagation.c
@@ -1,3 +1,14 @@
 #include "back_propagation.h"
 
 
+gradient* back_propagation(matrix* x, matrix* y, Parameters* p, activation* A)
+{
+    int m = y->col;
+
+    gradient* g = (gradient*)malloc(sizeof(gradient));
+
+    g->nb_layers = p->num_layers;
+
+    g->grad = (matrix**)malloc(g->nb_layers* sizeof(matrix));
+    
+}
