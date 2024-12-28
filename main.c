e2ad902359c99c4b41c0ceff21e74603fddd9bd6
Belhaddad dania <belhaddad2023@gmail.com>
2024-12-28 01:42:18 +0100

    affichage des poid

--- main.c
+++ main.c
@@ -27,26 +27,27 @@ int main() {
 
     Parameters* params = initialisation(2, 1, 2, 2);
 
+     matrix* gradient = createMatrix(1, 1);
 
-    for (int epoch = 0; epoch < epochs; epoch++) {
+    for (int epoch = 0; epoch < epochs; epoch++) 
+    {
         double total_log_loss = 0.0;
 
+         double gradient_norm = 0.0; 
+
         /*testtttttttt*/
-        matrix* gradient = createMatrix(1, 1); 
+        
 
         for (int i = 0; i < 100; i++) {
 
 
             matrix* x_sample=createMatrix(2,1);
-            
             x_sample->values[0][0]=X->values[i][0];
             x_sample->values[1][0]=X->values[i][1];
 
            
 
             activation* activations=sigmoid(x_sample,params);
-
-
             matrix* output = activations->activ[activations->nb_layers-1];
            
             
@@ -54,12 +55,14 @@ int main() {
             
 
             double output_gradient = (output->values[0][0] - Y->values[i][0]) * sigmoid_deriv(output)->values[0][0];
-            printf("output_gradient: %.5f\n", output_gradient);
 
 
             //matrix* gradient = createMatrix(1, 1);
+
             gradient->values[0][0] = output_gradient;
             update_weights(params->weight[1], gradient, learning_rate);
+            printf("Poids après mise à jour [1]: %.5f\n", params->weight[1]->values[0][0]);
+            printf("Poids après mise à jour [2]: %.5f\n", params->weight[1]->values[1][0]);
 
             params->bias[1]->values[0][0] -= learning_rate * output_gradient;
 
@@ -73,18 +76,17 @@ int main() {
             }
 
             update_weights(params->weight[0], hidden_gradient, learning_rate);
+            printf("Poids après mise à jour [hidden]: %.5f\n", params->weight[0]->values[0][0]);
+            
+
+            gradient_norm += gradient->values[0][0] * gradient->values[0][0];
 
             
         }
 
 
         /*testttttttttttttttttttttttt*/
-        double gradient_norm = 0.0;
-        for (int i = 0; i < gradient->row; i++) {
-            for (int j = 0; j < gradient->col; j++) {
-                gradient_norm += gradient->values[i][j] * gradient->values[i][j];
-            }
-        }
+        
         
         
 
@@ -93,6 +95,9 @@ int main() {
             printf("Norme des gradients à l'époque %d, échantillon %d: %.5f\n", epoch,  sqrt(gradient_norm));
         }
 
+        /*testttttt*/
+
+        gradient_norm = 0.0;
         
           
     }
