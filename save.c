14aa3f88b3390c2cd0f3d397ed8f929c4fec9d65
Belhaddad dania <belhaddad2023@gmail.com>
2024-12-28 21:08:56 +0100

    changement fonction save_weight_bias

--- save.c
+++ save.c
@@ -2,17 +2,40 @@
 
 void save_weight_bias(Parameters* params, const char* fichier)
 {
-    FILE* f = fopen(fichier, "wb");
+    FILE* f = fopen(fichier, "w");
     if (f == NULL) 
     {
-        printf("Erreur lors de l'ouverture du fichier pour sauvegarder les poids.\n");
+        printf("Erreur lors de l'ouverture du fichier pour sauvegarder en CSV.\n");
         return;
     }
 
     for (int i = 0; i < params->num_layers; i++) 
     {
-        fwrite(params->weight[i]->values, sizeof(double), params->weight[i]->row * params->weight[i]->col, f);
-        fwrite(params->bias[i]->values, sizeof(double), params->bias[i]->row * params->bias[i]->col, f);
+        
+        for (int r = 0; r < params->weight[i]->row; r++) 
+        {
+            for (int c = 0; c < params->weight[i]->col; c++)
+            {
+                fprintf(f, "%lf", params->weight[i]->values[r][c]);
+                if (c < params->weight[i]->col - 1) fprintf(f, ","); 
+            }
+
+            fprintf(f, "\n");
+        }
+
+        
+        for (int r = 0; r < params->bias[i]->row; r++)
+        {
+            for (int c = 0; c < params->bias[i]->col; c++)
+            {
+                fprintf(f, "%lf", params->bias[i]->values[r][c]);
+                if (c < params->bias[i]->col - 1) fprintf(f, ","); 
+
+            }
+            fprintf(f, "\n");
+        }
+
+        fprintf(f, "\n");
     }
 
     fclose(f);
