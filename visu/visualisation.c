2e414163966f345b3b204345cfb1b438ebed7ee9
Neila <berkaneneila2005@gmail.com>
2024-12-28 10:16:18 +0100

    fonction ligne changement

--- visu/visualisation.c
+++ visu/visualisation.c
@@ -1,11 +1,9 @@
-#include <SDL2/SDL.h> 
-#include <stdio.h>   
-#include<math.h> 
+#include <SDL2/SDL.h>
+#include <stdio.h>
+#include <math.h>
 
-
-
-void Circle(SDL_Renderer* renderer,int CX,int CY,int rayon){
-    for(int i=0; i<360; i++){
+void Circle(SDL_Renderer* renderer, int CX, int CY, int rayon) {
+    for (int i = 0; i < 360; i++) {
         double angle = i * M_PI / 180.0;
         int x = CX + rayon * cos(angle);
         int y = CY + rayon * sin(angle);
@@ -13,71 +11,84 @@ void Circle(SDL_Renderer* renderer,int CX,int CY,int rayon){
     }
 }
 
+void drawLine(SDL_Renderer* renderer, int x1, int y1, int x2, int y2) {
+    int dx = abs(x2 - x1);
+    int dy = abs(y2 - y1);
+    int sx = (x1 < x2) ? 1 : -1;
+    int sy = (y1 < y2) ? 1 : -1;
+    int err = dx - dy;
 
-
-void drawLine(SDL_Renderer* renderer, int x1, int y1, int x2, int y2){
-    SDL_RenderDrawLine(renderer, x1, y1, x2, y2);
-    
+    while (1) {
+        SDL_RenderDrawPoint(renderer, x1, y1);
+        if (x1 == x2 && y1 == y2) break;
+        int e2 = 2 * err;
+        if (e2 > -dy) {
+            err -= dy;
+            x1 += sx;
+        }
+        if (e2 < dx) {
+            err += dx;
+            y1 += sy;
+        }
+    }
 }
 
-
-
 int main(int argc, char* argv[]) {
-    
-
-    if (SDL_Init(SDL_INIT_VIDEO) < 0) { 
-        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError()); 
-        return -1; 
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+        return -1;
     }
 
-    
-    SDL_Window* window = SDL_CreateWindow("Neural network",            
-                                        SDL_WINDOWPOS_CENTERED,  
-                                        SDL_WINDOWPOS_CENTERED,  
-                                        800,   /*largeur et hauteur */                    
-                                        600,                       
-                                        SDL_WINDOW_SHOWN);        
+    SDL_Window* window = SDL_CreateWindow("Neural network",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          800,
+                                          600,
+                                          SDL_WINDOW_SHOWN);
 
-    
-    if (!window) { 
-        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError()); 
-        SDL_Quit(); 
-        return -1; 
+    if (!window) {
+        printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
     }
-    
+
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    
     if (!renderer) {
-        printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError()); 
-        SDL_DestroyWindow(window);  
-        SDL_Quit(); 
-        return -1; 
+        printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
     }
-    
+
     SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
     SDL_RenderClear(renderer);
 
-    Circle(renderer,400,300,100);
-    drawLine(renderer, 100, 100, 700, 100);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    Circle(renderer, 400, 300, 100);
+    Circle(renderer,200,300,20);
+    Circle(renderer,600,300,20);
+    Circle(renderer,400,500,20);
+    Circle(renderer,400,100,20);
+
+    
+
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    drawLine(renderer, 100, 100, 700, 500);
 
     SDL_RenderPresent(renderer);
 
-    
-    SDL_Event e; /*pour garder la fenetre ouverte */
-    int quit = 0;  
-
-    /*j'ai utiliser cette boucle pour que la fenetre n'est pas de temps limitee */
-    while (!quit) { 
-        while (SDL_PollEvent(&e) != 0) { 
-            if (e.type == SDL_QUIT) {   
-                quit = 1;  
+    SDL_Event e;
+    int quit = 0;
+    while (!quit) {
+        while (SDL_PollEvent(&e) != 0) {
+            if (e.type == SDL_QUIT) {
+                quit = 1;
             }
         }
     }
 
-    
-    SDL_DestroyWindow(window);  /*detruire la fenetre*/
-    SDL_Quit();                  
+    SDL_DestroyWindow(window);
+    SDL_Quit();
 
-    return 0; 
+    return 0;
 }
