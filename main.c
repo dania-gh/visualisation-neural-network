ef769e01c7b1b769af23205427813a94ba6adb1e
Belhaddad dania <belhaddad2023@gmail.com>
2024-12-28 02:06:39 +0100

    supp de gradiant_norm

--- main.c
+++ main.c
@@ -27,15 +27,15 @@ int main() {
 
     Parameters* params = initialisation(2, 1, 2, 2);
 
-     matrix* gradient = createMatrix(1, 1);
+    
+
+    
 
     for (int epoch = 0; epoch < epochs; epoch++) 
     {
         double total_log_loss = 0.0;
+        
 
-         double gradient_norm = 0.0; 
-
-        /*testtttttttt*/
         
 
         for (int i = 0; i < 100; i++) {
@@ -57,15 +57,12 @@ int main() {
             double output_gradient = (output->values[0][0] - Y->values[i][0]) * sigmoid_deriv(output)->values[0][0];
 
 
-            //matrix* gradient = createMatrix(1, 1);
+            matrix* gradient = createMatrix(1, 1);
 
             gradient->values[0][0] = output_gradient;
             update_weights(params->weight[1], gradient, learning_rate);
 
-            printf("Dimensions des poids[1] : %d x %d\n", params->weight[1]->row, params->weight[1]->col);
-
-            printf("Poids après mise à jour [1]: %.5f\n", params->weight[1]->values[0][0]);
-            printf("Poids après mise à jour [2]: %.5f\n", params->weight[1]->values[0][1]);
+            
 
             params->bias[1]->values[0][0] -= learning_rate * output_gradient;
 
@@ -79,29 +76,12 @@ int main() {
             }
 
             update_weights(params->weight[0], hidden_gradient, learning_rate);
-            printf("Poids après mise à jour [hidden]: %.5f\n", params->weight[0]->values[0][0]);
-            
-
-            gradient_norm += gradient->values[0][0] * gradient->values[0][0];
-
-            
+                      
         }
-
-
-        /*testttttttttttttttttttttttt*/
-        
-        
-        
 
         if (epoch % 1000 == 0) {
             printf("Epoch %d, Log Loss moyen : %.4f\n", epoch, total_log_loss / 100);
-            printf("Norme des gradients à l'époque %d, échantillon %d: %.5f\n", epoch,  sqrt(gradient_norm));
         }
-
-        /*testttttt*/
-
-        gradient_norm = 0.0;
-        
           
     }
 
