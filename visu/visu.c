e12128d30006d7c5efc5ed67bc31e1382d799a6f
Ferielysf <ysfferielqgmail.com>
2025-01-01 21:30:41 +0100

    add before training part

--- visu/visu.c
+++ visu/visu.c
@@ -1,5 +1,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL2_gfxPrimitives.h>
+#include <SDL2/SDL_ttf.h>
 #include <stdio.h>
 int width = 1800;
 int height = 950 ;
@@ -18,8 +19,6 @@ SDL_Quit();
 return 1;
 }
 
-
-
 SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 if (!renderer) {
 printf("Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
@@ -28,12 +27,91 @@ SDL_Quit();
 return 1;
 }
 
+/*if (TTF_Init() == -1) {
+    printf("TTF_Init: %s\n", TTF_GetError());
+    SDL_Quit();
+    return 1;
+}*/
+
+int r =25;
+Uint32 startTime2 = SDL_GetTicks();
+SDL_Event e; /*pour garder la fenetre ouverte */
+int quit = 0;
+while (!quit && SDL_GetTicks()<= startTime2+6000 ) { 
+while (SDL_PollEvent(&e) != 0) { 
+if (e.type == SDL_QUIT) {   
+quit = 1;  
+}
+}
+    Uint32 startTime1 = SDL_GetTicks();
+    while(SDL_GetTicks() < startTime1+3000)
+    {
+        SDL_SetRenderDrawColor(renderer, 30, 30, 30, 255);
+        SDL_RenderClear(renderer);
+
+        
+
+        for (int i = 100; i <= 880; i += 80)
+        {
+        for (int j = 100; j <= 880; j += 80) {
+        lineRGBA(renderer, 600, i, 900, j, i, 100, 200, 255); //line between fist and second layers
+        lineRGBA(renderer, 900, i, 1200, j, 100, 200, i, 255);//line between second and third layers
+        }
+        }
+        for (int i = 100; i <= 880; i += 80) {
+        for(int j = 200 ; j<=800;j+=80)
+        {
+            lineRGBA(renderer, 300, j, 600, i, 204, 0, 0, 255);//lines between input and fist layer
+        }
+
+
+
+        lineRGBA(renderer, 1200, i, 1500, height / 2, 0,128,255, 255); //line between last layers and output
+
+        filledCircleRGBA(renderer, 600, i, r, 30, 30, 30, 255); //first layer background
+        filledCircleRGBA(renderer, 900, i, r, 30, 30, 30, 255);//second layer background
+        filledCircleRGBA(renderer, 1200, i, r, 30, 30, 30, 255);//third layer background
+
+
+        filledCircleRGBA(renderer, 600, i, r, i, 100, 200, 255); //first layer
+        filledCircleRGBA(renderer, 900, i, r, 100, 200, i, 255);//second layer
+        filledCircleRGBA(renderer, 1200, i, r, 100,i, 200, 255);//third layer
+        }
+        for(int i = 200 ; i<=800;i+=80 ){ //input node
+            filledCircleRGBA(renderer, 300, i, r, 30, 30, 30, 255);
+            filledCircleRGBA(renderer, 300,i, r, 204, 0, 0, 255);
+        }
+
+
+        //output node
+        filledCircleRGBA(renderer, 1500, height/2, r, 30, 30, 30, 255);
+        filledCircleRGBA(renderer, 1500, height/2, r, 0,128,255, 255);
+
+
+
+
+        SDL_RenderPresent(renderer);
+
+    }
+
+
+    startTime1 = SDL_GetTicks();
+    while(SDL_GetTicks() < startTime1+3000)
+    {
+        for(int i = 200 ; i<=800;i+=80 ){ //input node
+        filledCircleRGBA(renderer, 300, i, r+10, 30, 30, 30, 255);
+        filledCircleRGBA(renderer, 300,i, r+10, 204, 0, 0, 255);
+        }
+        SDL_RenderPresent(renderer);
+    }
+}
+
 int alpha1, alpha2,alpha3,alpha4,alpha5;
 
 Uint32 startTime = SDL_GetTicks();
 Uint32 duration = 1000;
-SDL_Event e; /*pour garder la fenetre ouverte */
-int quit = 0;
+
+quit = 0;
 while (!quit) { 
 while (SDL_PollEvent(&e) != 0) { 
 if (e.type == SDL_QUIT) {   
@@ -42,7 +120,7 @@ quit = 1;
 }
 
 Uint32 elapsedTime = SDL_GetTicks() - startTime;
-if (elapsedTime > 9 * duration ) {
+if (elapsedTime > 8 * duration ) {
 startTime = SDL_GetTicks();
 elapsedTime = 0;
 }
