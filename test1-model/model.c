cc0d2e761ccb09824f4b98255b8fa13d90f51df4
Belhaddad dania <belhaddad2023@gmail.com>
2025-01-02 21:54:45 +0100

    ajout boucle pour teste les input_normalise_trainig

--- test1-model/model.c
+++ test1-model/model.c
@@ -17,7 +17,7 @@ int main ()
 
     matrix* input_normalise_training = createMatrix(500 , 8);
     matrix* output_training = createMatrix(500, 1);
-    matrix* output_normalise_predect = createMatrix(500,1);
+    matrix* output_training_predect = createMatrix(500,1);
 
     creat_input_normalise_test_Y_Z_W(input_normalise_test, output_test,input_normalise_training , output_training );
 
@@ -25,7 +25,8 @@ int main ()
 
     load_weight_bias(params, "weight.csv", "bias.csv");
 
-    for (int i = 0; i < 267; i++) {
+    for (int i = 0; i < 267; i++)
+    {
         
 
         matrix* input_normalise_test_sample  = createMatrix(8, 1);
@@ -42,13 +43,34 @@ int main ()
 
         matrix* output = activations->activ[params->num_layers - 1];
 
-
-    
-        printf("Entrées : %.1f, %.1f | Sortie calculée : %.2f | Sortie attendue : %.1f\n",
-        input_normalise_test->values[i][0], input_normalise_test->values[i][1], output->values[0][0], output_test->values[i][0]);
-        
-
+        output_test_predect->values=output->values;
     }
+
+
+    for (int i = 0; i < 500; i++)
+    {
+        matrix* input_normalise_training_sample = createMatrix(8,1);
+        input_normalise_training_sample->values[0][0]=input_normalise_training->values[i][0];
+        input_normalise_training_sample->values[1][0]=input_normalise_training->values[i][1];
+        input_normalise_training_sample->values[2][0]=input_normalise_training->values[i][2];
+        input_normalise_training_sample->values[3][0]=input_normalise_training->values[i][3];
+        input_normalise_training_sample->values[4][0]=input_normalise_training->values[i][4];
+        input_normalise_training_sample->values[5][0]=input_normalise_training->values[i][5];
+        input_normalise_training_sample->values[6][0]=input_normalise_training->values[i][6];
+        input_normalise_training_sample->values[7][0]=input_normalise_training->values[i][7];
+
+        activation* activations = sigmoid(input_normalise_training_sample, params);
+
+        matrix* output = activations->activ[params->num_layers - 1];
+        
+        output_training_predect->values=output->values;
+    }
+
+
 
     return 0;
 }
+
+
+/*printf("Entrées : %.1f, %.1f | Sortie calculée : %.2f | Sortie attendue : %.1f\n",
+        input_normalise_test->values[i][0], input_normalise_test->values[i][1], output->values[0][0], output_test->values[i][0]);*/
